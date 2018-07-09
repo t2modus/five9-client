0d088458b622578f3b5f2185ab1532499daf6f72
Andrew Stephenson <Andrew.Stephenson123@gmail.com>
2018-07-09 13:23:57 -0600

    Added more classes to the default require'

--- lib/five9/client.rb
+++ lib/five9/client.rb
@@ -1,6 +1,12 @@
 require 'client/version'
 require 'client/configuration'
 require 'client/base'
+require 'client/campaign'
+require 'client/list'
+require 'client/logger'
+require 'client/login'
+require 'client/disposition'
+require 'client/report'
 
 module Five9
   # This class is responsible for providing a reliable interface to
